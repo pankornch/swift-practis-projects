6839c6d2408e0b401766409bf5e15295bf11600a
Pankorn Chantarod <pankorn.ch@gmail.com>
2023-04-29 23:59:00 +0700

    initial commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Pankorn on 29/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
